9f669e9caa5cdee89b2c1df0cc956b63b4b43b11
Kalvin <kalvinhart@hotmail.co.uk>
2023-08-15 19:49:44 +0100

    added dashboard and jobs routes

---
+++ src/pages/MyJobs/MyJobs.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyJobs: React.FC = () => {
+  return <div>MyJobs</div>;
+};
+
+export default MyJobs;
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,14 +2,26 @@ import React from "react";
 import { Routes as RoutesList, Route } from "react-router-dom";
 import { LoginPage } from "../pages/LoginPage";
 import { PageLayout } from "../layouts/PageLayout";
+import { Dashboard } from "../pages/Dashboard";
+import { MyJobs } from "../pages/MyJobs";
 
 const Routes: React.FC = () => {
   return (
     <RoutesList>
+      <Route
+        path="/login"
+        Component={LoginPage}
+      />
+
       <Route Component={PageLayout}>
         <Route
-          path="/login"
-          Component={LoginPage}
+          path="/jobs"
+          Component={MyJobs}
+        />
+
+        <Route
+          path="/"
+          Component={Dashboard}
         />
       </Route>
     </RoutesList>
